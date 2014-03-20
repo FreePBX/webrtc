853219a12c9de64afc1175698e73eb04dca03bef
Andrew Nagy <andrew.nagy@the159.com>
2014-03-19 21:51:28 -0400

    FREEPBX-7228 #QA #comment Allow creation of WebRTC phone on add Extension page, also allow creation on pjsip extensions (should not matter)

--- functions.inc/hook_core.php
+++ functions.inc/hook_core.php
@@ -1,37 +1,31 @@
 <?php
 if (!defined('FREEPBX_IS_AUTH')) { die('No direct script access allowed'); }
 
-function webrtc_configpageinit($dispnum) {
+function webrtc_configpageinit($pagename) {
 	global $currentcomponent;
-	switch ($dispnum) {
-		case 'extensions':
-			if (isset($_REQUEST['tech_hardware']) && (
-				$_REQUEST['tech_hardware'] == 'sip_generic' ||
-					$_REQUEST['tech_hardware'] == 'iax2_generic' ||
-				$_REQUEST['tech_hardware'] == 'dahdi_generic' )) {
-					$extdisplay = '';
-			} elseif (!isset($_REQUEST['parkpro_parkinglot']) && !isset($_REQUEST['extdisplay'])) {
-				return true;
-			} else {
-				$device_info = core_devices_get($_REQUEST['extdisplay']);
-				if (empty($device_info) || $device_info['tech'] != 'sip' && $device_info['tech'] != 'iax2' && $device_info['tech'] != 'dahdi') {
-					return true;
-				}
-			}
-		break;
-	    default;
-	  		return true;
-	    break;
+	global $amp_conf;
+
+	$action = isset($_REQUEST['action'])?$_REQUEST['action']:null;
+	$extdisplay = isset($_REQUEST['extdisplay'])?$_REQUEST['extdisplay']:null;
+	$extension = isset($_REQUEST['extension'])?$_REQUEST['extension']:null;
+	$tech_hardware = isset($_REQUEST['tech_hardware'])?$_REQUEST['tech_hardware']:null;
+	$supported_hardware = array('sip','pjsip','iax2','dahdi');
+
+    // We only want to hook the 'extensions' pages.
+	if ($pagename != 'extensions' && in_array(str_replace('_generic','',$_REQUEST['tech_hardware']),$supported_hardware))  {
+		return true;
 	}
-	
-	$currentcomponent->addoptlistitem('webrtc_enable', 'no', 'No');
-	$currentcomponent->addoptlistitem('webrtc_enable', 'yes', 'Yes');
-	$currentcomponent->setoptlistopts('webrtc_enable', 'sort', false);
 
-  	$currentcomponent->addguifunc("webrtc_{$dispnum}_configpageload");
+	if ($tech_hardware != null || $extdisplay != '' || $action == 'add') {
+		$currentcomponent->addoptlistitem('webrtc_enable', 'no', 'No');
+		$currentcomponent->addoptlistitem('webrtc_enable', 'yes', 'Yes');
+		$currentcomponent->setoptlistopts('webrtc_enable', 'sort', false);
 
-	if (!empty($_REQUEST['action'])) {
-		$currentcomponent->addprocessfunc("webrtc_{$dispnum}_configprocess");
+		$currentcomponent->addguifunc("webrtc_{$pagename}_configpageload");
+
+		if (!empty($action)) {
+			$currentcomponent->addprocessfunc("webrtc_{$pagename}_configprocess");
+		}
 	}
 }
 
@@ -46,35 +40,38 @@ function webrtc_extensions_configpageload() {
 function webrtc_configpageload($mode) {
 	global $amp_conf;
 	global $currentcomponent;
-	
+
 	$extdisplay = isset($_REQUEST['extdisplay'])?$_REQUEST['extdisplay']:null;
 
 	$webrtc_select = $currentcomponent->getoptlist('webrtc_enable');
-	
+
 	$webrtc_value = webrtc_get_enabled($extdisplay);
-	
+
 	if(version_compare($amp_conf['ASTVERSION'],'11.5')) {
 		$currentcomponent->addguielem('WebRTC Phone', new gui_selectbox( 'webrtc_enable', $webrtc_select, $webrtc_value,
 		  _('Enable WebRTC User Control Panel Phone'), sprintf(_('Enable User Panel WebRTC Phone Client for this %s'),$mode), false));
 	} else {
 		$currentcomponent->addguielem('Device Options', new gui_hidden('webrtc_enable', $webrtc_value));
-	}	  
+	}
 }
 
 function webrtc_extensions_configprocess() {
-	switch ($_REQUEST['action']) {
+	$action = isset($_REQUEST['action'])?$_REQUEST['action']:null;
+	$extension = isset($_REQUEST['extdisplay'])?$_REQUEST['extdisplay']:null;
+	switch ($action) {
 		case 'add':
+			$extension = isset($_REQUEST['extension']) ? $_REQUEST['extension'] : null;
 		case 'edit':
-			$prev =  webrtc_get_enabled($_REQUEST['extension']);
-			webrtc_set_enabled($_REQUEST['extension'], $_REQUEST['webrtc_enable']);
+			$prev =  ($action != 'add') ? webrtc_get_enabled($extension) : 'no';
+			webrtc_set_enabled($extension, $_REQUEST['webrtc_enable']);
 			if($_REQUEST['webrtc_enable'] == 'yes' && $prev == 'no') {
-				webrtc_create_device($_REQUEST['extension']);
+				webrtc_create_device($extension);
 			} elseif($_REQUEST['webrtc_enable'] == 'no' && $prev == 'yes') {
-				webrtc_delete_device($_REQUEST['extension']);
+				webrtc_delete_device($extension);
 			}
 		break;
 		case 'del':
-			webrtc_delete($_REQUEST['extdisplay']);
+			webrtc_delete($extension);
 		break;
 	}
 }
