dcb85bbefcf4d355f3ec2d3c3a081e5dc4e9a56e
James Finstrom <james.finstrom@schmoozecom.com>
2014-12-22 17:06:35 -0300

    Bootstrap stuff

--- views/ucp_config.php
+++ views/ucp_config.php
@@ -1,7 +1,28 @@
-<span class="radioset">
-	<input type="radio" name="webrtc|enable" id="webrtc|enable_yes" value="yes" <?php echo ($enabled) ? 'checked' : ''?>>
-	<label for="webrtc|enable_yes">Yes</label>
-	<input type="radio" name="webrtc|enable" id="webrtc|enable_no" value="no" <?php echo !($enabled) ? 'checked' : ''?>>
-	<label for="webrtc|enable_no">No</label>
-</span>
-<br/>
+<!--ucp_config.php-->
+<div class="element-container">
+	<div class="row">
+		<div class="col-md-12">
+			<div class="row">
+				<div class="form-group">
+					<div class="col-md-3">
+						<label class="control-label" for="webrtcenable"><?php echo _("Enable WebRTC Phone") ?></label>
+						<i class="fa fa-question-circle fpbx-help-icon" data-for="webrtcenable"></i>
+					</div>
+					<div class="col-md-9 radioset">
+						<input type="radio" name="webrtc|enable" id="webrtc|enable_yes" value="yes" <?php echo ($enabled) ? 'checked' : ''?>>
+						<label for="webrtc|enable_yes"><?php echo _("Yes")?></label>
+						<input type="radio" name="webrtc|enable" id="webrtc|enable_no" value="no" <?php echo !($enabled) ? 'checked' : ''?>>
+						<label for="webrtc|enable_no"><?php echo _("No")?></label>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-md-12">
+			<span id="webrtcenable-help" class="help-block fpbx-help-block"><?php echo _("Enable the WebRTC Phone in UCP for this user")?></span>
+		</div>
+	</div>
+</div>
+<!--ucp_config.php-->
+
--- views/ucp_config_originate.php
+++ views/ucp_config_originate.php
@@ -1,7 +1,28 @@
-<span class="radioset">
-	<input type="radio" name="webrtc|originate" id="webrtc|originate_yes" value="yes" <?php echo ($enabled) ? 'checked' : ''?>>
-	<label for="webrtc|originate_yes">Yes</label>
-	<input type="radio" name="webrtc|originate" id="webrtc|originate_no" value="no" <?php echo !($enabled) ? 'checked' : ''?>>
-	<label for="webrtc|originate_no">No</label>
-</span>
-<br/>
+<!--ucp_config_originate.php-->
+<div class="element-container">
+	<div class="row">
+		<div class="col-md-12">
+			<div class="row">
+				<div class="form-group">
+					<div class="col-md-3">
+						<label class="control-label" for="webrtcoriginate"><?php echo _("Enable WebRTC Originate") ?></label>
+						<i class="fa fa-question-circle fpbx-help-icon" data-for="webrtcoriginate"></i>
+					</div>
+					<div class="col-md-9 radioset">
+						<input type="radio" name="webrtc|originate" id="webrtc|originate_yes" value="yes" <?php echo ($enabled) ? 'checked' : ''?>>
+						<label for="webrtc|originate_yes"><?php echo _("Yes")?></label>
+						<input type="radio" name="webrtc|originate" id="webrtc|originate_no" value="no" <?php echo !($enabled) ? 'checked' : ''?>>
+						<label for="webrtc|originate_no"><?php _("No")?></label>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-md-12">
+			<span id="webrtcoriginate-help" class="help-block fpbx-help-block"><?php echo _("Enable the WebRTC Originate in UCP for this user")?></span>
+		</div>
+	</div>
+</div>
+<!--ucp_config_originate.php-->
+
