e1713fcdca36cb99416a4c192cfa0d3f30251e53
Andrew Nagy <andrew.nagy@the159.com>
2018-01-16 17:28:31 -0500

    fix tests

--- utests/InstallVersionTest.php
+++ utests/InstallVersionTest.php
@@ -22,10 +22,10 @@ class InstallVersionTest extends PHPUnit_Framework_TestCase {
 	public function test14Install() {
 		$c = self::$f->Config();
 		self::$current = $c->get('ASTVERSION');
-		$c->set_conf_values(array('ASTVERSION' => "15.0.2"),true,true);
+		$c->set_conf_values(array('ASTVERSION' => "16.0.2"),true,true);
 		// This should be a string
-		$this->assertTrue((self::$f->Webrtc->validVersion() !== true), "15.0.2 is not a valid version, but it's saying it is");
-		$c->set_conf_values(array('ASTVERSION' => "14.0.2"),true,true);
-		$this->assertTrue(self::$f->Webrtc->validVersion(), "14.0.2 is a valid version, but its saying it is not");
+		$this->assertTrue((self::$f->Webrtc->validVersion() !== true), "16.0.2 is not a valid version, but it's saying it is");
+		$c->set_conf_values(array('ASTVERSION' => "15.0.2"),true,true);
+		$this->assertTrue(self::$f->Webrtc->validVersion(), "15.0.2 is a valid version, but its saying it is not");
 	}
 }
