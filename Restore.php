b89bb06ca9b825ba2508f56d6c6426b832ff1365
Andrew Nagy <andrew.nagy@the159.com>
2019-02-11 13:38:24 -0500

    backup and restore

--- Restore.php
+++ Restore.php
@@ -3,7 +3,7 @@ namespace FreePBX\modules\Webrtc;
 use FreePBX\modules\Backup as Base;
 class Restore Extends Base\RestoreBase{
   public function runRestore($jobid){
-    $configs = reset($this->getConfigs());
+    $configs = $this->getConfigs();
     $this->FreePBX->Webrtc->setMultiConfig($configs['settings']);
     foreach ($configs['clients'] as $client) {
         $this->FreePBX->Webrtc->upsertClientSettings($client['user'], $client['device'], $client['certid']);
