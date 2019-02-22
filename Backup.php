d8a65fa7f8fd2a24312455a7bad6eb8474699c56
Andrew Nagy <andrew.nagy@the159.com>
2019-02-21 23:39:10 -0500

    [Module Tag script: webrtc 15.0.5] fixing legacy restore

--- Backup.php
+++ Backup.php
@@ -2,13 +2,13 @@
 namespace FreePBX\modules\Webrtc;
 use FreePBX\modules\Backup as Base;
 class Backup Extends Base\BackupBase{
-  public function runBackup($id,$transaction){
-    $configs = [
-        'settings'  => $this->FreePBX->Webrtc->getAll(),
-        'clients' => $this->FreePBX->Webrtc->getClientsEnabled(),
-    ];
-    $this->addDependency('userman');
-    $this->addDependency('certman');
-    $this->addConfigs($configs);
-  }
+	public function runBackup($id,$transaction){
+		$configs = [
+				'tables'	=> $this->dumpTables(),
+				'kvstore' => $this->dumpKVStore(),
+		];
+		$this->addDependency('userman');
+		$this->addDependency('certman');
+		$this->addConfigs($configs);
+	}
 }
--- Restore.php
+++ Restore.php
@@ -2,32 +2,12 @@
 namespace FreePBX\modules\Webrtc;
 use FreePBX\modules\Backup as Base;
 class Restore Extends Base\RestoreBase{
-  public function runRestore($jobid){
-    $configs = $this->getConfigs();
-    $this->FreePBX->Webrtc->setMultiConfig($configs['settings']);
-    foreach ($configs['clients'] as $client) {
-        $this->FreePBX->Webrtc->upsertClientSettings($client['user'], $client['device'], $client['certid']);
-    }
-  }
+	public function runRestore($jobid){
+		$configs = $this->getConfigs();
+		$this->importTables($configs['tables']);
+	}
 
-  public function processLegacy($pdo, $data, $tables, $unknownTables, $tmpfiledir){
-    $tables = array_flip($tables+$unknownTables);
-    if(!isset($tables['webrtc_users'])){
-      return $this;
-    }
-    $bmo = $this->FreePBX->Webrtc;
-    $bmo->setDatabase($pdo);
-    $configs = [
-      'settings' => $bmo->getAll(),
-      'clients' => $bmo->getClientsEnabled(),
-    ];
-    $bmo->resetDatabase();
-    foreach ($configs['clients'] as $client) {
-      $bmo->upsertClientSettings($client['user'], $client['device'], $client['certid']);
-    }
-    $this->transformLegacyKV($pdo, 'webrtc', $this->FreePBX)
-      ->transformNamespacedKV($pdo, 'webrtc', $this->FreePBX);
-    return $this;
-  }
-
+	public function processLegacyKvstore($pdo, $data, $tables, $unknownTables){
+		$this->restoreLegacyDatabase($pdo);
+	}
 }
